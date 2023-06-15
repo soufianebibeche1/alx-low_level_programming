964867af565e995290d65a3e7ccf6dd11bdc394f
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-15 14:38:57 -0700

    5-printf. script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - debut programme
+ *
+ * Description: a C Programme displays an output using the printf function.
+ *
+ * Return: Always 0 which mean everything runs correct
+*/
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
