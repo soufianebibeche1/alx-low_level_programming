4e198703d3ba0cc6f34d4998bd9f6389ce407e8c
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-15 15:32:56 -0700

    101-quote.c script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: A C program that prints a specific
+ * message to the standard error.
+ *
+ * Return: 1 as a successful execution.
+ */
+int main(void)
+{
+	char message[] = "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, message, strlen(message));
+
+	return (1);
+}
+
