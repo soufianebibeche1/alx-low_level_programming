7c207a82499ff7e8b6e8d13bf8ee2cb54c510777
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-15 14:16:58 -0700

    4-puts.c source

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-/*
- * Main - a basic programme write what is between puts 
+/**
+ * Main Function
+ *
+ * This program displays an output using the puts function.
+ *
+ * Return: 0 upon successful execution.
  */
+
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
