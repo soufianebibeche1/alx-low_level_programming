4a8d9fe78a9e5ac4f87699dddd62caa29ea12c26
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-15 15:41:35 -0700

    101-quote.c source

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,8 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *message = "and that piece of art is use
+		ful\" - Dora Korpar, 2015-10-19\n";
 
 	write(0, message, strlen(message));
 
