1c738187b89e11604da4a7f648be2eaa7c8d92fd
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-15 15:56:13 -0700

    101-quote.c script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,10 +12,10 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is use
-		ful\" - Dora Korpar, 2015-10-19\n";
+	const char *message;
 
-	write(0, message, strlen(message));
+	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, message, strlen(message));
 
 	return (1);
 }
