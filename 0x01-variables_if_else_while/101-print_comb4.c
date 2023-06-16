0f66d7c3c7b62a7773ff1ea93a2746a2fb0263f5
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-16 13:47:48 -0700

    latest projects

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: A C program that prints all possible
+ *
+ * different combinations of three digits.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 10; num2++)
+		{
+			for (num3 = num2 + 1; num3 < 10; num3++)
+			{
+				if (num1 != num2 && num1 != num3 && num2 != num3)
+				{
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(num3 + '0');
+
+					if (num1 != 7 || num2 != 8 || num3 != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
