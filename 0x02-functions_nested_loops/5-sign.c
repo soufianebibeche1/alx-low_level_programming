dde48f0cd8374a18ead87715cdb4d73656745f50
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-20 15:23:34 -0700

    adding an update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 
-/* Function: alpha a function that checks for alphan character */
-
+/**
+ * print_sign - checks for number sign.
+ *
+ * Description: This function checks for number sign.
+ *
+ * @n: The number to be checked.
+ *
+ * Return: 1 if n is > 0, -1 if n is < 0 otherwise 0.
+ */
 int print_sign(int n)
 {
 if (n > 0)
