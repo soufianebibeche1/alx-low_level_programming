c170ad752f836ce62a44e7a38823c6a888d99739
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-21 12:11:20 -0700

    natural numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,34 +3,30 @@
 /**
  * main - Entry point of the program
  *
- * Description: prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line
+ * Description: prints the first 50 Fibonacci numbers,
+ *
+ * starting with 1 and 2, followed by a new line
  *
  * Return: Always 0 (Success)
  */
-
-int printfibonacci(int n)
-{
-	int fibunaccinumb;
-	
-	if (n == 0)
-	{
-		fibunaccinumb = 0;
-	}
-	else if (n == 1)
-	{
-		fibunaccinumb = 1;
-	}
-	else
-	{
-		fibunaccinumb = printfibonacci(n - 1) + printfibonacci(n - 2);
-	}
-	return (fibunaccinumb);
-}
 int main(void)
 {
-	int n;
+	int fib0, fib1, fibn, i;
 
-	n = printfibonacci(50);
+	fib0 = 0;
+	fib1 = 1;
+
+	printf("%d,%d", fib0, fib1);
+	for (i = 2; i < 50; i++)
+	{
+		fibn = fib1 + fib0;
+		printf(",%d", fibn);
+
+		fib0 = fib1;
+		fib1 = fibn;
+
+	}
+
 	printf("\n");
 
 	return (0);
