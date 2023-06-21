3df12e6ab2f7dc00f40f95ce818ae7dce536c790
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-20 19:28:18 -0700

    more fixe to done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@
 void times_table(void)
 {
 	int num, mult, prod;
-	
+
 	for (num = 0; num <= 9; ++num)
 	{
 		_putchar(48);
@@ -15,15 +15,13 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
-		
 			prod = num * mult;
-		
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + 48); /*get the first digit*/
-			
-			_putchar((prod % 10) + 48); /*get the second digit*/
+				_putchar((prod / 10) + 48);
+
+		_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
