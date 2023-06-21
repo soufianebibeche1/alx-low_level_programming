6ed1cb8e8717203bed02741e1c931f238893e78d
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-20 17:31:42 -0700

    adding more fixes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -17,17 +16,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-	putchar('+');
+	_putchar('+');
 	return (1);
 }
 else if (n < 0)
 {
-	putchar('-');
+	_putchar('-');
 	return (-1);
 }
 else
 {
-	putchar('0');
+	_putchar('0');
 	return (0);
 }
 }
