f5f9d75abc4945a2f0b8404ace39f30ccf100f1b
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-20 19:12:30 -0700

    addin fiuxe

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,6 +2,7 @@
 
 /**
  * print_last_digit - Prints and returns the last digit of a number.
+ *
  * @n: The number to get the last digit from.
  *
  * Return: The value of the last digit.
@@ -11,9 +12,13 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
-		n = -n;
-
-	last_digit = n % 10;
+	{
+		last_digit = -1 * (n % 10);
+	}
+	else
+	{
+		last_digit = n % 10;
+	}
 	_putchar(last_digit + '0');
 
 	return (last_digit);
