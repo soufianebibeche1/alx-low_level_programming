0ac8658733581178f8b7bc9183ece0b36c2903fd
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-21 19:01:22 -0700

    the last one

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,35 +1,27 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Description: prints the first 50 Fibonacci numbers,
- *
- * starting with 1 and 2, followed by a new line
- *
- * Return: Always 0 (Success)
- */
+void print_fibonacci(int count);
+
 int main(void)
 {
-	int i;
-	unsigned int fib1, fib2, fibn;
-
-	fib1 = 1;
-	fib2 = 2;
-
-	printf("%u, %u", fib1, fib2);
-
-	for (i = 3; i <= 98; i++)
-	{
-		fibn = (unsigned int)fib2 + fib1;
-		printf(", %u", fibn);
-
-		fib1 = fib2;
-		fib2 = fibn;
-	}
-
-	printf("\n");
-
-	return (0);
+	print_fibonacci(98);
+	return 0;
 }
 
+void print_fibonacci(int count)
+{
+	int i, fib1 = 1, fib2 = 2, fib_next;
+  
+	printf("%d, %d", fib1, fib2);
+  
+	for (i = 3; i <= count; ++i)
+	{
+		fib_next = fib1 + fib2;
+		printf(", %d", fib_next);
+        
+		fib1 = fib2;
+		fib2 = fib_next;
+	}
+  
+	printf("\n");
+}
