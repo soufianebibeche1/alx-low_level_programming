37a8615450f10da56a6b6c83cbaded734f8eb800
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-21 17:21:34 -0700

    debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -11,5 +11,18 @@
  */
 void positive_or_negative(int i)
 {
-	printf("%d is positive\n", i);
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+
+	else
+	{
+		printf("%d is negative\n", i);
+	}
 }
