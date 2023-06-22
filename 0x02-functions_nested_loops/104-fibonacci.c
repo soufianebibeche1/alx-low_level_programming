57ca1ef0c56e057830864fe5a46f26eebf035c40
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-21 19:40:46 -0700

    the last

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,42 +4,41 @@ unsigned long fibonacci(unsigned int n);
 
 int main(void)
 {
-    unsigned int num_terms, i;
+	unsigned int num_terms, i;
+	
+	&num_terms = 98;
+	
+	printf("Fibonacci sequence:\n");
+	for (i = 0; i < num_terms; i++)
+	{
+		printf("%lu ", fibonacci(i));
+	}
 
-    printf("Enter the number of terms in the Fibonacci sequence: ");
-    scanf("%u", &num_terms);
-
-    printf("Fibonacci sequence:\n");
-    for (i = 0; i < num_terms; i++)
-    {
-        printf("%lu ", fibonacci(i));
-    }
-
-    return 0;
+	return 0;
 }
 
 unsigned long fibonacci(unsigned int n)
 {
-    if (n == 0)
-    {
-        return 0;
-    }
-    else if (n == 1)
-    {
-        return 1;
-    }
-    else
-    {
-        unsigned long a = 0, b = 1, c;
-        unsigned int i;
+	if (n == 0)
+	{
+		return 0;
+	}
+	else if (n == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		unsigned long a = 0, b = 1, c;
+		unsigned int i;
 
-        for (i = 2; i <= n; i++)
-        {
-            c = a + b;
-            a = b;
-            b = c;
-        }
+		for (i = 2; i <= n; i++)
+		{
+			c = a + b;
+			a = b;
+			b = c;
+		}
 
-        return b;
+		return b;
     }
 }
