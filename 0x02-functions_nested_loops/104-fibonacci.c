10484f7c9a8b5f76eb6078cdabe2897b9ede08b6
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-21 19:10:36 -0700

    the last one

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
 
-void print_fibonacci(int n) {
-    int i, num1 = 1, num2 = 2, nextNum;
-
-    printf("%d, %d, ", num1, num2); // Print the first two Fibonacci numbers
-
-    for (i = 3; i <= n; i++) {
-        nextNum = num1 + num2;
-        printf("%d", nextNum);
-
-        if (i != n) {
-            printf(", "); // Print comma and space if it's not the last number
-        }
-
-        num1 = num2;
-        num2 = nextNum;
-    }
-
-    printf("\n");
+/**
+ * print_comma_space - Prints a comma followed by a space
+ */
+void print_comma_space(void)
+{
+	printf(", ");
 }
 
-int main() {
-    print_fibonacci(98);
-    return 0;
+/**
+ * print_fibonacci - Prints the Fibonacci sequence up to a given number
+ * @n: The number of Fibonacci sequence to print
+ */
+void print_fibonacci(int n)
+{
+	int i, a = 1, b = 1, next;
+
+	printf("1");
+
+	for (i = 2; i <= n; i++)
+	{
+		printf("%d", a);
+		if (i != n)
+			print_comma_space();
+
+		next = a + b;
+		a = b;
+		b = next;
+	}
+
+	printf("\n");
 }
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int limit;
+
+	printf("Enter the limit for Fibonacci sequence: ");
+	scanf("%d", &limit);
+
+	print_fibonacci(limit);
+
+	return 0;
+}
+
