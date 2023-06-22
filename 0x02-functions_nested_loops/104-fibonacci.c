e0b09bf8ab1c74d9a484535a990cd4201c284459
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-21 19:21:21 -0700

    the last one

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,45 +1,30 @@
 #include <stdio.h>
 
-/**
- * print_fibonacci - Prints the first 98 Fibonacci numbers
- */
-void print_fibonacci(void)
-{
-	int i;
-	unsigned int fibonacci[100] = {0}; // Array to store the Fibonacci numbers
+#define MAX 100
 
-	fibonacci[98] = 1; // Initialize the last element as 1
-	fibonacci[97] = 2; // Initialize the second last element as 2
+long long fib_cache[MAX];
 
-	// Calculate Fibonacci numbers using big number addition
-	for (i = 96; i >= 0; i--)
-	{
-		fibonacci[i] += fibonacci[i + 1] / 1000000000; // Carry-over to the next digit
-		fibonacci[i + 1] %= 1000000000; // Keep only the last 9 digits
+long long fibonacci_memoization(int n) {
+    if (n <= 1)
+        return n;
 
-		fibonacci[i] += fibonacci[i + 2]; // Perform addition
-	}
+    if (fib_cache[n] != -1)
+        return fib_cache[n];
 
-	// Print the Fibonacci numbers
-	for (i = 0; i < 99; i++)
-	{
-		printf("%u", fibonacci[i]);
-
-		if (i != 98)
-			printf(", ");
-	}
-
-	printf(" and more\n");
+    fib_cache[n] = fibonacci_memoization(n - 1) + fibonacci_memoization(n - 2);
+    return fib_cache[n];
 }
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-	print_fibonacci();
+int main() {
+    int i;
 
-	return 0;
+    // Initialize cache with -1
+    for (i = 0; i < MAX; i++)
+        fib_cache[i] = -1;
+
+    // Print the first 98 Fibonacci numbers
+    for (i = 1; i <= 98; i++)
+        printf("%lld\n", fibonacci_memoization(i));
+
+    return 0;
 }
