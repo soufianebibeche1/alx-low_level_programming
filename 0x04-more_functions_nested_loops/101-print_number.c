17bb4b993fff2c6fc6cee4b5a127de334ac98cc4
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-23 16:09:02 -0700

    fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,9 @@
  */
 void print_number(int n)
 {
+	int num_digits = 0;
+	int temp, divisor, i;
+	
 	if (n == 0)
 	{
 		_putchar('0');
@@ -18,8 +21,24 @@ void print_number(int n)
 		n = -n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	num_digits = 0;
+	temp = n;
 
-	_putchar((n % 10) + '0');
+	while (temp != 0)
+	{
+		temp /= 10;
+		num_digits++;
+	}
+
+	divisor = 1;
+
+	for ( i = 1; i < num_digits; i++)
+		divisor *= 10;
+
+	while (divisor != 0)
+	{
+		_putchar((n / divisor) + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
 }
