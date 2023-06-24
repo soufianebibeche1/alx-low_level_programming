ec32b350ac0d939d9a9c1b5dba50e722d5f44339
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-23 18:17:20 -0700

    fixing errors

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
 
-void calculateFibonacci(int n);
-
 int main(void) {
-    int n = 98;
-    calculateFibonacci(n);
-
-    return 0;
-}
-
-void calculateFibonacci(int n) {
-    int fib0 = 0, fib1 = 1, fibn, i;
-    char arr[200]; // Increased the size to accommodate the long string
-
-    sprintf(arr, "%d, %d", fib0, fib1); // Store the first two Fibonacci numbers in the array
-    printf("%s", arr); // Print the array
-
-    for (i = 2; i < n; i++) {
-        fibn = fib0 + fib1;
-        printf(", %d", fibn);
-        sprintf(arr, "%s, %d", arr, fibn);
-        fib0 = fib1;
-        fib1 = fibn;
+    int count;
+    unsigned int fib1 = 1, fib2 = 2, fibn;
+    
+    printf("%d, %d", fib1, fib2);  // Print the first two Fibonacci numbers
+    
+    for (count = 3; count <= 98; count++) {
+        fibn = fib1 + fib2;
+        printf(", %u", fibn);
+        
+        fib1 = fib2;
+        fib2 = fibn;
     }
-
+    
     printf("\n");
+    
+    return 0;
 }
