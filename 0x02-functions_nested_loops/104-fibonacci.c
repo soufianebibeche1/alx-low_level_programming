444731c8bcd66a0e9d482cbd7b8dd6934519b607
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-23 18:28:26 -0700

    fixing errors

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - Entry point of the program
+ *
+ * Description: Finds and prints the sum of the even-valued terms
+ *              in the Fibonacci sequence, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     int count;
-    unsigned int fib1 = 1, fib2 = 2, fibn;
-    
-    printf("%d, %d", fib1, fib2);  // Print the first two Fibonacci numbers
-    
-    for (count = 3; count <= 98; count++) {
-        fibn = fib1 + fib2;
-        printf(", %u", fibn);
-        
+    long fib1 = 1, fib2 = 2, fibn = 0;
+    long sum = 2; // Start with the sum as 2 (for the second Fibonacci number)
+
+    printf("%ld, %ld", fib1, fib2); // Print the first two Fibonacci numbers
+
+    while (count <= 96) // Loop until the 96th Fibonacci number
+    {
+        fibn = fib2 + fib1;
+
+        if (fibn % 2 == 0) // Check if the Fibonacci number is even
+        {
+            sum += fibn; // Add the even Fibonacci number to the sum
+        }
+
+        printf(", %ld", fibn);
+
         fib1 = fib2;
         fib2 = fibn;
+
+        count++;
     }
-    
-    printf("\n");
-    
     return 0;
 }
+
