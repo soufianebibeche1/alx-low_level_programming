7c793ce1b0d53f176c8d0b5d78b442a1a4072beb
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-26 16:43:31 -0700

    strcpy code

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_strcpy - prints n elements of an array
+ *
+ * Description : a function that copies the string pointed to by src
+ *
+ * @dest: the destination.
+ *
+ * @src: the source.
+ *
+ * Return: char.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, length;
+
+	length = strlen(src);
+	for (i = 0; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[length + 1] = '\0';
+
+	return (dest);
+}
