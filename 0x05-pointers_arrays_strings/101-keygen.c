eba551e3ecf87c0b7520391dae2fb13d0687c647
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-27 09:21:11 -0700

    test

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,42 @@
+ #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 11
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char password[PASSWORD_LENGTH + 1];
+    int i;
+
+    srand(time(NULL));
+
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        int randomCharType = rand() % 3;
+        switch (randomCharType)
+        {
+		
+            case 0:
+                password[i] = 'A' + rand() % 26;
+                break;
+            case 1:
+                password[i] = 'a' + rand() % 26;
+                break;
+            case 2:
+                password[i] = '0' + rand() % 10;
+                break;
+        }
+    }
+
+    password[PASSWORD_LENGTH] = '\0';
+
+    printf("%s\n", password);
+
+    return 0;
+}
