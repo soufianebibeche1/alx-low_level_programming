635e966a8cd47918229e034fd081b2b0b6459499
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-27 16:57:03 -0700

    tesr

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 100
-#define MAX_CHAR_VALUE 127
-
 /**
  * main - check the code
  *
@@ -12,19 +9,17 @@
  */
 int main(void)
 {
-    int password[PASSWORD_LENGTH];
-    int sum = 0;
-    int i;
+	int sum;
+	char c;
+	
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
 
-    srand(time(NULL));
-
-    for (i = 0; i < PASSWORD_LENGTH - 1; i++) {
-        password[i] = rand() % (MAX_CHAR_VALUE + 1);
-        sum += password[i];
-        putchar(password[i] + '0');
-    }
-
-    putchar((2772 - sum) + '0');
-
-    return 0;
+    return (0);
 }
