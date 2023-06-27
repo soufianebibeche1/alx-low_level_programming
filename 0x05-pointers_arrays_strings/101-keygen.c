240b38b83a3c8f6f454f7d1e82d5662890bfc31a
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-27 14:53:42 -0700

    adding a test

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    int password[16];
-    int i;
+	int sum;
+	char charcater;
 
-    srand(time(NULL));
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		charcater = rand() % 120;
+		sum += charcater;
+		putchar(charcater);
+	}
 
-    for (i = 0; i < 15; i++) {
-        password[i] = rand() % 94 + 33; // Generate a random ASCII value between 33 and 126
-        putchar(password[i]);
-    }
+	putchar(2772 - sum);
 
-    password[15] = '\0'; // Null-terminate the password
-
-    return 0;
+	return (0);
 }
