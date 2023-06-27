4edd5253d37a334d7fa45ff48f768537b1ccbc19
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-26 19:34:36 -0700

    adding

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 /**
@@ -13,30 +11,33 @@
  */
 int _atoi(char *s)
 {
-	int length = strlen(s);
+	int i = 0;
+	unsigned int result = 0;
 	int sign = 1;
-	long int result = 0;
-	int i;
+	int digit = 0;
 
-    for (i = 0; i < length; i++)
-    {
-        if (*(s + i) == ' ')
-        {
-            continue;
-        }
-        else if (*(s + i) == '-')
-        {
-            sign = -1;
-        }
-        else if (*(s + i) == '+')
-        {
-            continue;
-        }
-        else if (*(s + i) >= '0' && *(s + i) <= '9')
-        {
-            result = result * 10 + (s[i] - '0');
-        }
-    }
+	while (s[i])
+	{
+		if (s[i] == 45)
+		{
+			sign *= -1;
+		}
 
-    return (result * sign);
+		while (s[i] >= 48 && s[i] <= 57)
+		{
+			digit = 1;
+			result = (result * 10) + (s[i] - '0');
+			i++;
+		}
+
+		if (digit == 1)
+		{
+			break;
+		}
+
+		i++;
+	}
+
+	result = result * sign;
+	return (result);
 }
