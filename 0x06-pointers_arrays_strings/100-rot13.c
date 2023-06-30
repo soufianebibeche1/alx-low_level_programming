6e78adc8b49653e4133b262a59c984780226befb
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-30 15:44:03 -0700

    testnew

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,24 +9,25 @@
  *
  * @str: The string.
  *
- * Return: the new string encoded.
+ * Return: the new string
  */
 char *rot13(char *str)
 {
-	int i, j;
+	int i = 0, j;
 	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot13_alphabet[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*(str + i) != '\0')
 	{
-		for (j = 0; strlen(alphabet); j++)
+		for (j = 0; j < (int)strlen(alphabet); j++)
 		{
-			if (alphabet[j] == str[i])
+			if (*(str + i) == alphabet[j])
 			{
-				str[i] = rot13_alphabet[j];
-				break;
+				*(str + i) = rot13[j];
 			}
+
 		}
+		i++;
 	}
 	return (str);
 }
