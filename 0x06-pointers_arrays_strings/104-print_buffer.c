347c9335142681944122849cef4f9c380460b93b
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-06-30 20:05:10 -0700

    test

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -15,23 +15,35 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int j, k, l;
 
 	if (size <= 0)
-	{
 		printf("\n");
-		return;
-	}
-
-	for (i = 0; i < size; i += 10)
+	else
 	{
-		printf("%08x: ", i);
-		for (j = 0; j < 10 && i + j < size; j++)
-			printf("%02x%s", b[i + j], (j % 2) ? " " : "");
-
-		for (j = 0; j < 10 && i + j < size; j++)
-			printf("%c", (b[i + j] >= 32 && b[i + j] <= 126) ? b[i + j] : '.');
-
-		printf("\n");
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+				printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
 	}
-
+}
