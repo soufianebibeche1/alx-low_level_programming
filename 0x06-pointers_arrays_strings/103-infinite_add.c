2dcd1698b9354b07af5d4a384aacf3ec3114a19c
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-07-01 07:39:46 -0700

    ya

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -19,31 +19,44 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int count1, count2, carry = 0, digit1, digit2, sum = 0, bigger_size;
+	int len1 = 0, len2 = 0, sum, maxLen, digit1, digit2, add = 0;
 
-	for (count1 = 0; n1[count1] != '\0'; count1++);
-	for (count2 = 0; n2[count2] != '\0'; count2++);
-
-	bigger_size = (count1 >= count2) ? count1 : count2;
-	
-	if (size_r <= bigger_size + 1)
-		return NULL;
-
-	r[bigger_size + 1] = '\0';
-	count1--, count2--, size_r--;
-
-	while (bigger_size >= 0)
+	while (*(n1 + len1) != '\0')
+		len1++;
+	while (*(n2 + len2) != '\0')
+		len2++;
+	if (len1 >= len2)
+		maxLen = len1;
+	else
+		maxLen = len2;
+	if (size_r <= maxLen + 1)
+		return (0);
+	r[maxLen + 1] = '\0';
+	len1--, len2--, size_r--;
+	digit1 = *(n1 + len1) - 48, digit2 = *(n2 + len2) - 48;
+	while (maxLen >= 0)
 	{
-		digit1 = (count1 >= 0) ? n1[count1--] - '0' : 0;
-		digit2 = (count2 >= 0) ? n2[count2--] - '0' : 0;
-
-		sum = digit1 + digit2 + carry;
-		carry = sum / 10;
-		*(r + bigger_size--) = (sum % 10) + '0';
+		sum = digit1 + digit2 + add;
+		if (sum >= 10)
+			add = sum / 10;
+		else
+			add = 0;
+		if (sum > 0)
+		*(r + maxLen) = (sum % 10) + 48;
+		else
+			*(r + maxLen) = '0';
+		if (len1 > 0)
+			len1--, digit1 = *(n1 + len1) - 48;
+		else
+			digit1 = 0;
+		if (len2 > 0)
+			len2--, digit2 = *(n2 + len2) - 48;
+		else
+			digit2 = 0;
+		maxLen--, size_r--;
 	}
-
-	if (*r == '0')
+	if (*(r) == '0')
 		return (r + 1);
 	else
-		return r;
+		return (r);
 }
