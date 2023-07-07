0e09709941258349920eb6d6cf8ebc061f7f181a
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-07-07 16:09:52 -0700

    adding test

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Description : a program that print its own name.
+ *
+ * @argc: The number of command line arguments
+ * @argv: An array of command line arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	int i, count;
+
+
+	for (i = 0; i < argc; i++)
+	{
+		count = i;
+
+	}
+	printf("%d\n", count);
+	return (0);
+}
