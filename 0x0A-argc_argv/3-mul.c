7eaa452f9bdca185d75185d35d8c8c34d71c66f9
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-07-07 16:27:04 -0700

    another test

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Description : a program that multiplies two numbers.
+ *
+ * @argc: The number of command line arguments
+ * @argv: An array of command line arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int n1, n2, mul;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		mul = n1 * n2;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
