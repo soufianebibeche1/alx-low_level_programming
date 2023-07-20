08db29b483c91d37a16979b1be21c247636dc009
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-07-20 14:19:09 -0700

    test

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		arg = va_arg(args, int);
 		printf("%d", arg);
 
-		if (separator != NULL)
+		if (i < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
