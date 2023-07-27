09ba5cd437c39aa3a931fb37923d2bbad943455f
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-07-27 15:58:23 -0700

    advanced one

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * exec_before_main - execute before main that prints aphrase
+ * @void: program variable.
+ *
+ * Return: void
+ */
+void exec_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
