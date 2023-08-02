fce412322e1386a7e9e02e0ea65da398a72879ef
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-08-02 11:19:42 -0700

    test

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,15 +10,20 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
-	listint_t *next;
+	listint_t *temp, *x;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	temp = *head;
 
 	while (temp != NULL)
 	{
-		next = temp->next;
-		free(temp);
-		temp = next;
+		x = temp;
+		temp = temp->next;
+		free(x);
 	}
-
 	*head = NULL;
 }
