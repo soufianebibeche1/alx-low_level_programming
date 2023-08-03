bd54543983b7a6db08e0bf995ae2a84efc776e0e
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-08-03 09:59:03 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: the converted number or 0, or 0 if
+ * there is one or more chars in the
+ * string b that is not 0 or 1 b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, res = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		res = 2 * res + (b[i] - '0');
+	}
+	return (res);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+Project: Bit Manipulation in C
+This project focuses on learning how to manipulate bits and use bitwise operators in the C programming language. The tasks in this project will help you understand how to work with individual bits within integers and perform various operations to achieve specific results.
