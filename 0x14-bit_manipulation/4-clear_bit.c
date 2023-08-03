9672f9e7431af8b877ce97caf215fe8ac3731037
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-08-03 12:23:54 -0700

    task 5

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -17,5 +17,5 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	}
 	*n = (~(1UL << index) & *n);
 
-	return (1);	
+	return (1);
 }
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * flip_bits - the number of bits you would need
+ * to flip to get from one number to another.
+ *
+ * @n: The first unsigned long integer.
+ * @m: The second unsigned long integer.
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int test = n ^ m;
+	unsigned int cpt = 0;
+
+	while (test != 0)
+	{
+		cpt += test & 1;
+		test >>= 1;
+	}
+	return (cpt);
+}
