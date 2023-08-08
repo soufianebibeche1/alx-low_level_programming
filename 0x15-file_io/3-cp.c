1404e096f370323127a7a7b0785ec54ec0c378d3
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-08-08 13:56:53 -0700

    test2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,74 +6,53 @@
 
 #define BUFFER_SIZE 1024
 
-
 /**
  * main - checks the code.
+ *
+ * @argc: The number of arguments.
+ * @argv: An array of strings containing the arguments.
  *
  * Return: 0 succes.
  */
 int main(int argc, char *argv[])
 {
-	const char *src_file, *new_file;
-	int fp_src, fp_new;
+	const char *src, *dest;
+	int fd_src, fd_dest;
 	ssize_t bytes_read, bytes_written;
-	char buff[BUFFER_SIZE];
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s src_file new_file\n", argv[0]);
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: %s src_file dest_file\n", argv[0]);
+		return (97);
 	}
-
-	src_file = argv[1];
-	new_file = argv[2];
-
-	fp_src = open(src_file, O_RDONLY);
-	if (fp_src == -1)
+	src = argv[1];
+	dest = argv[2];
+	fd_src = open(src, O_RDONLY);
+	fd_dest = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_src == -1 || fd_dest == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_file);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't open or create files\n");
+		return (98);
 	}
-
-	fp_new = open(new_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fp_new == -1)
+	while ((bytes_read = read(fd_src, buffer, BUFFER_SIZE)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", new_file);
-		close(fp_src);
-		exit(99);
-	}
-
-	while ((bytes_read = read(fp_src, buff, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fp_new, buff, bytes_read);
+		bytes_written = write(fd_dest, buffer, bytes_read);
 		if (bytes_written == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", new_file);
-			close(fp_src);
-			close(fp_new);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+			return (99);
 		}
 	}
-
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_file);
-		close(fp_src);
-		close(fp_new);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		return (98);
 	}
-
-	if (close(fp_src) == -1)
+	if (close(fd_src) == -1 || close(fd_dest) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp_src);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close file descriptors\n");
+		return (100);
 	}
-
-	if (close(fp_new) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp_new);
-		exit(100);
-	}
-
 	return (0);
 }
